3fdbe31e225f628164db4ec2dd9513beaeea69f1
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-11-14 16:25:36 -0500

    can now  access stripe

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import axios from "axios"
 
 function App() {
+
+  const handleCheckout = async () => {
+    let baseURL = 'http://localhost:4000'
+    let queryUrl = baseURL + "/create-checkout-session"
+    let reqBody = { items: [
+      { id: 1, quantity: 2 }, 
+      { id: 2, quantity: 3 }
+    ]}
+    // By default, if the 2nd parameter to axios.post() is an object, Axios serializes the object to JSON using the JSON.stringify() function.
+    // so, you can often omit the headers argument, if all you care about is the content type 
+    // axios.post(url, body, headers)
+    let newData = await axios.post(queryUrl, reqBody)
+    console.log('newData', newData) 
+    // if the request is successful, redirect the user 
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Get Stripe Stuff</p>
+      <button onClick={handleCheckout}>Checkout</button>
     </div>
   );
 }
