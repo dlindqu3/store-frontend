de4caf03281d5d668a3a7fd9db38b6daa157899b
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-12-09 12:13:14 -0500

    can create new cart if it is first item selected or add items if cart already exists or increment up quantity

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,57 +4,19 @@ import axios from "axios";
 function ProductCard({ productData, currentUserId, currentToken }) {
 
   let handleAddToCart = async (productData) => {
-    // console.log("id of item added to cart: ", productData._id);
-    // console.log("current user's id: ", currentUserId);
 
     // check if there is a cart for given user
     let cartExistsUrl = "http://localhost:4000/" + "api/cart/read-cart/" + currentUserId;
-    let resData = await axios.get(cartExistsUrl, {
+    let cartData = await axios.get(cartExistsUrl, {
       headers: { Authorization: `Bearer ${currentToken}` },
     });
-    // works down to here
     
     console.log('productData: ', productData)
-    console.log('resData: ', resData)
+    console.log('cartData: ', cartData)
 
-    if (resData.data[0]) {
-      // console.log("cart exists: ", resData.data[0]);
+    if (!cartData.data[0]){
 
-      // if current product already in cart, increment quantity by 1
-      for (let i = 0; i < resData.data[0].cartItems.length; i++){
-        let currentObj = resData.data[0].cartItems[i]
-        let hasProduct = Object.values(currentObj).includes(productData._id)
-        if (hasProduct){
-          console.log('clicked product already in cart')
-          let currentDbCartItems = resData.data[0].cartItems
-          for (let i = 0; i < currentDbCartItems.length; i++){
-            let currentObj = currentDbCartItems[i]
-            if (currentObj['product'] === productData._id){
-              currentObj['quantity'] += 1 
-            }
-          }
-          console.log('updated resData before posting to db: ', resData)
-          // works to here 
-
-          let cartId = resData.data[0]._id
-          let updateCartUrl = "http://localhost:4000/" + "api/cart/update-cart/" + cartId
-          let updatedCartData = await axios.put(updateCartUrl, resData.data[0], {
-            headers: { Authorization: `Bearer ${currentToken}` },
-          });
-
-          console.log('updatedCartData: ', updatedCartData)
-
-        } else {
-          console.log('clicked product not in cart')
-        }
-      }
-  
-
-      // else if current product not in cart, add current product to cart with quantity of 1
-
-    } else {
-      console.log("cart does not exist");
-      // if no cart, create one with current product
+      console.log('there is no cart')
       let createCartUrl = "http://localhost:4000/" + "api/cart/create-cart/";
       let createCartBody = {
         user: currentUserId,
@@ -69,9 +31,69 @@ function ProductCard({ productData, currentUserId, currentToken }) {
         headers: { Authorization: `Bearer ${currentToken}` },
       });
       console.log('createdCart: ', createdCart)
-    }
-  };
 
+
+    } else if (cartData.data[0]) {
+      console.log('there is a cart')
+      console.log('cartItems: ', cartData.data[0].cartItems)
+ 
+      let currentProductInCart = false; 
+      let existingProductIdx; 
+
+      for (let i = 0; i < cartData.data[0].cartItems.length; i++){
+        let currentObj = cartData.data[0].cartItems[i]
+        let hasProduct = Object.values(currentObj).includes(productData._id)
+        if (hasProduct){
+          currentProductInCart = true
+          existingProductIdx = i; 
+          console.log('currentObj with product that is in cart: ', currentObj)
+        }
+      }
+      // console.log('currentProductInCart: ', currentProductInCart)
+
+      // if current product is in cart, increment its quantity by 1
+      if (currentProductInCart){
+        cartData.data[0].cartItems[existingProductIdx].quantity += 1
+
+          let cartId = cartData.data[0]._id
+          let updateCartUrl = "http://localhost:4000/" + "api/cart/update-cart/" + cartId
+         
+          let updatedCartData = await axios.patch(updateCartUrl, cartData.data[0], {
+            headers: { Authorization: `Bearer ${currentToken}` },
+          });
+
+          console.log('updatedCartData: ', updatedCartData)
+
+      }
+      
+
+      // if current product is not in cart, add it to cart with quantity of 1
+      if (!currentProductInCart){
+        
+        console.log('cart exists, but clicked product not in cart')
+        
+        let newItem = {
+          product: productData._id,
+          quantity: 1,
+        }
+        cartData.data[0].cartItems.push(newItem)
+        
+        let cartId = cartData.data[0]._id
+        let updateCartUrl = "http://localhost:4000/" + "api/cart/update-cart/" + cartId
+
+        let updatedCartData = await axios.patch(updateCartUrl, cartData.data[0], {
+          headers: { Authorization: `Bearer ${currentToken}` },
+        });
+
+
+        console.log('updated cart Data after new item added: ', updatedCartData)
+      
+      } 
+
+    }
+}
+
+  // from cart page: 
   // let updateCart = ...
   // let createNewCart = ...
 
