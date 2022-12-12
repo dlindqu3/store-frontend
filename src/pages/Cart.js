c1ec3bf4963f9728d51a68c6197eb71c938d6d16
Dwight Lindquist <dwight.lindquist@gmail.com>
2022-12-12 09:48:40 -0500

    can handle conditional cart message

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,27 +4,25 @@ import CartCard from "../components/CartCard";
 
 function Cart({ currentToken, currentUserId }) {
   const [cart, setCart] = useState(); 
-  const [cartExists, setCartExists] = useState(); 
   const [products, setProducts] = useState();
-
-  let checkCartExists = async () => {
-    let cartExistsUrl =
-      "http://localhost:4000/" + "api/cart/read-cart/" + currentUserId;
-    let cartData = await axios.get(cartExistsUrl, {
-      headers: { Authorization: `Bearer ${currentToken}` },
-    });
-    if (cartData.data[0]) {
-    setCart(cartData.data[0])
-    setCartExists(true)
-    }
-    setCartExists(false)
-  };
-
 
 
   useEffect(() => {
-    
-    // checkCartExists sets state for cart if it exists 
+
+    let checkCartExists = async () => {
+      
+      let cartExistsUrl =
+        "http://localhost:4000/" + "api/cart/read-cart/" + currentUserId;
+      
+        let cartData = await axios.get(cartExistsUrl, {
+        headers: { Authorization: `Bearer ${currentToken}` },
+      });
+      if (cartData.data[0]) {
+        console.log('cartData.data: ', cartData.data)
+        setCart(cartData.data[0])
+      }
+    };
+  
     checkCartExists()
     
   }, []);
@@ -33,6 +31,7 @@ function Cart({ currentToken, currentUserId }) {
   return (
     <div>
       <h2>Cart Page</h2>
+      {!cart && <p>Your cart is empty.</p>}
       {cart && cart.cartItems.map((item) => {
         return <CartCard key={item.product} productId={item.product} quantity={item.quantity}/> 
       }) }
